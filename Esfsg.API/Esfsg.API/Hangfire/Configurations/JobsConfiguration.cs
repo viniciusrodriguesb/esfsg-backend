dc5b26aa7eb63044a0284e58bf30b63fba678703
vinic <viniciusrbastos@hotmail.com>
2025-06-18 09:14:45 -0300

    Feat: preparação job. Alteração do CRON.

--- Esfsg.API/Esfsg.API/Hangfire/Configurations/JobsConfiguration.cs
+++ Esfsg.API/Esfsg.API/Hangfire/Configurations/JobsConfiguration.cs
@@ -15,39 +15,39 @@ namespace Esfsg.API.Hangfire.Configurations
             jobManager.AddOrUpdate<EmailInscricaoConfirmadaJob>(
                   "email-inscricao-confirmada",
                   job => job.Execute(),
-                  Cron.Yearly);
+                         "*/5 * * * *");
 
             jobManager.AddOrUpdate<EmailQrCodeAcessoJob>(
                "email-qrcode-pagamento",
                job => job.Execute(),
-               Cron.Yearly);
+                      "*/5 * * * *");
 
             jobManager.AddOrUpdate<EmailQrCodePagamentoJob>(
                "email-qrcode-acesso",
                job => job.Execute(),
-               Cron.Yearly);
+                      "*/5 * * * *");
 
             jobManager.AddOrUpdate<EmailCancelamentoJob>(
                "email-cancelamento",
                job => job.Execute(),
-               Cron.Yearly);
+                      "*/5 * * * *");
 
             jobManager.AddOrUpdate<EmailReembolsoJob>(
                "email-reembolso",
                job => job.Execute(),
-               Cron.Yearly);
+                      "*/5 * * * *");
             #endregion
 
             #region Pagamento
             jobManager.AddOrUpdate<GerarPagamentoJob>(
                  "gerar-pagamento",
                  job => job.Execute(),
-                 Cron.Hourly);
+                        "*/5 * * * *");
 
             jobManager.AddOrUpdate<AlteraStatusInscricaoPagamentoJob>(
               "alterar-status-pagamento",
               job => job.Execute(),
-              Cron.Daily); 
+                     "*/5 * * * *");
             #endregion
 
         }
