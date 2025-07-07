25857efd0a4e2b3261d87178b414eb496dcbc492
vinic <viniciusrbastos@hotmail.com>
2025-07-07 16:01:01 -0300

    Feat: inserção do idEvento no checkin.

--- Esfsg.API/Esfsg.Application/DTOs/Request/ConsultaCheckInRequest.cs
+++ Esfsg.API/Esfsg.Application/DTOs/Request/ConsultaCheckInRequest.cs
@@ -1,7 +1,11 @@
-﻿namespace Esfsg.Application.DTOs.Request
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Esfsg.Application.DTOs.Request
 {
     public class ConsultaCheckInRequest
     {
+        [Required(ErrorMessage = "ID do evento é obrigatório")]
+        public int IdEvento { get; set; }
         public string? Nome { get; set; }
         public string? Periodo { get; set; }
         public int? FuncaoEvento { get; set; }
