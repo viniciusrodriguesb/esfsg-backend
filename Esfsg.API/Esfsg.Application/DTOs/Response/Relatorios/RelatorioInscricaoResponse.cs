064a570cd6051ab1446a8b2cc9ff63d6cf3fc44a
vinic <viniciusrbastos@hotmail.com>
2025-07-16 17:00:38 -0300

    Feat: criação de endpoint de exportação de relatórios.

---
+++ Esfsg.API/Esfsg.Application/DTOs/Response/Relatorios/RelatorioInscricaoResponse.cs
@@ -0,0 +1,38 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Esfsg.Application.DTOs.Response.Relatorios
+{
+    public class RelatorioInscricaoResponse
+    {
+
+        [Display(Name = "Nome Completo")]
+        public string NomeCompleto { get; set; }
+
+        [Display(Name ="CPF")]
+        public string Cpf { get; set; }
+
+        [Display(Name ="Telefone")]
+        public string Telefone { get; set; }
+
+        [Display(Name ="Classe")]
+        public string Classe { get; set; }
+
+        [Display(Name ="Igreja")]
+        public string Igreja { get; set; }
+
+        [Display(Name ="Período")]
+        public string Periodo { get; set; }
+
+        [Display(Name ="Função no Evento")]
+        public string FuncaoEvento { get; set; }
+
+        [Display(Name ="Data da Inscrição")]
+        public string DhInscricao { get; set; }
+
+        [Display(Name ="Status da Inscrição")]
+        public string StatusInscricao { get; set; }
+
+        [Display(Name ="Presença")]
+        public string Presenca { get; set; }
+    }
+}
---
+++ Esfsg.API/Esfsg.Application/Helpers/FormatoHelper.cs
@@ -0,0 +1,33 @@
+﻿namespace Esfsg.Application.Helpers
+{
+    public static class FormatoHelper
+    {
+        public static string FormatarCpf(string cpf)
+        {
+            if (string.IsNullOrWhiteSpace(cpf))
+                return string.Empty;
+
+            var numeros = new string(cpf.Where(char.IsDigit).ToArray());
+
+            if (numeros.Length != 11)
+                return cpf; 
+
+            return Convert.ToUInt64(numeros).ToString(@"000\.000\.000\-00");
+        }
+
+        public static string FormatarTelefone(string telefone)
+        {
+            if (string.IsNullOrWhiteSpace(telefone))
+                return string.Empty;
+
+            var numeros = new string(telefone.Where(char.IsDigit).ToArray());
+
+            return numeros.Length switch
+            {
+                10 => Convert.ToUInt64(numeros).ToString(@"\(00\) 0000\-0000"),
+                11 => Convert.ToUInt64(numeros).ToString(@"\(00\) 00000\-0000"),
+                _ => telefone 
+            };
+        }
+    }
+}
